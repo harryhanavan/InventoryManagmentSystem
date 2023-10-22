1ce3668bcd647ab9236f2fc5593f540c2afd4b24
harryhanavan <harryhanavan@gmail.com>
2023-10-22 23:45:07 +1100

    v1.01
    Add view product, with filtering and search capabilities

---
+++ InventoryManagementSystem/ProductView.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class ProductView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewProducts = new DataGridView();
+            btnSearch = new Button();
+            txtSearch = new TextBox();
+            cmbCategory = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            cmbSupplier = new ComboBox();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewProducts
+            // 
+            dataGridViewProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProducts.Location = new Point(12, 145);
+            dataGridViewProducts.Name = "dataGridViewProducts";
+            dataGridViewProducts.RowTemplate.Height = 25;
+            dataGridViewProducts.Size = new Size(744, 198);
+            dataGridViewProducts.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(681, 100);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 23);
+            btnSearch.TabIndex = 1;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(501, 101);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(174, 23);
+            txtSearch.TabIndex = 2;
+            // 
+            // cmbCategory
+            // 
+            cmbCategory.FormattingEnabled = true;
+            cmbCategory.Items.AddRange(new object[] { "Electronics", "Clothing", "Groceries" });
+            cmbCategory.Location = new Point(12, 101);
+            cmbCategory.Name = "cmbCategory";
+            cmbCategory.Size = new Size(121, 23);
+            cmbCategory.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Category";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(159, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Filter by:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Below $50", "$50 - $100", "$100 - $150", "$150-$300", "$300+" });
+            comboBox1.Location = new Point(159, 100);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(205, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Price";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(344, 83);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Supplier";
+            // 
+            // cmbSupplier
+            // 
+            cmbSupplier.FormattingEnabled = true;
+            cmbSupplier.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+            cmbSupplier.Location = new Point(310, 101);
+            cmbSupplier.Name = "cmbSupplier";
+            cmbSupplier.Size = new Size(121, 23);
+            cmbSupplier.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(509, 83);
+            label5.Name = "label5";
+            label5.Size = new Size(166, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Search by Product ID or Name";
+            // 
+            // ProductView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(cmbSupplier);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(cmbCategory);
+            Controls.Add(txtSearch);
+            Controls.Add(btnSearch);
+            Controls.Add(dataGridViewProducts);
+            Name = "ProductView";
+            Text = "ProductView";
+            Load += ProductView_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewProducts;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private ComboBox cmbCategory;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private ComboBox cmbSupplier;
+        private Label label5;
+    }
+}
---
+++ InventoryManagementSystem/ProductView.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class ProductView : Form
+    {
+        public ProductView()
+        {
+            InitializeComponent();
+            LoadProductsIntoDataGridView();
+        }
+        private void ProductView_Load(object sender, EventArgs e)
+        {
+            LoadProductsIntoDataGridView();
+        }
+        private void LoadProductsIntoDataGridView()
+        {
+            try
+            {
+                // Load products from the data source
+                List<Product> products = Product.LoadProducts();
+
+
+                // Bind the products to the DataGridView
+                dataGridViewProducts.DataSource = products;
+
+                // Set column headers
+                dataGridViewProducts.Columns["ProductID"].HeaderText = "Product ID";
+                dataGridViewProducts.Columns["Name"].HeaderText = "Name";
+                dataGridViewProducts.Columns["Description"].HeaderText = "Description";
+                dataGridViewProducts.Columns["Category"].HeaderText = "Category";
+                dataGridViewProducts.Columns["Price"].HeaderText = "Price";
+                dataGridViewProducts.Columns["Quantity"].HeaderText = "Quantity";
+                dataGridViewProducts.Columns["SupplierID"].HeaderText = "Supplier ID";
+
+                // Format Price column as currency
+                dataGridViewProducts.Columns["Price"].DefaultCellStyle.Format = "c";
+
+                // Auto-resize columns
+                dataGridViewProducts.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+                // Set DataGridView as read-only
+                dataGridViewProducts.ReadOnly = true;
+                dataGridViewProducts.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading the products: {ex.Message}");
+            }
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            string searchTerm = string.IsNullOrWhiteSpace(txtSearch.Text) ? null : txtSearch.Text.ToLower();
+            string selectedCategory = cmbCategory.SelectedItem as string;
+            string selectedPriceRange = comboBox1.SelectedItem as string;
+            int selectedSupplierID = -1; // Default value
+            if (cmbSupplier.SelectedItem != null && int.TryParse(cmbSupplier.SelectedItem.ToString(), out int parsedSupplierID))
+            {
+                selectedSupplierID = parsedSupplierID;
+
+                Console.WriteLine($"Selected Supplier ID: {selectedSupplierID}"); // Debugging output
+            }
+            else
+            {
+                Console.WriteLine("No Supplier ID selected or failed to parse."); // Debugging output
+            }
+
+
+            List<Product> products = Product.LoadProducts();
+
+            var filteredProducts = products
+                .Where(p => string.IsNullOrEmpty(searchTerm) || p.ProductID.ToString().Contains(searchTerm) || p.Name.ToLower().Contains(searchTerm))
+                .Where(p => string.IsNullOrEmpty(selectedCategory) || p.Category.ToString() == selectedCategory)
+                .Where(p => string.IsNullOrEmpty(selectedPriceRange) || PriceInRange(p.Price, selectedPriceRange))
+                .Where(p => cmbSupplier.SelectedItem == null || (p.SupplierID == selectedSupplierID))
+                .ToList();
+
+            dataGridViewProducts.DataSource = filteredProducts;
+            // Refresh the DataGridView to reflect changes
+            dataGridViewProducts.Refresh();
+        
+    }
+
+        // You can create a method to handle the price range filtering
+        private bool PriceInRange(decimal price, string selectedPriceRange)
+        {
+            switch (selectedPriceRange)
+            {
+                case "Below $50":
+                    return price < 50;
+                case "$50 - $100":
+                    return price >= 50 && price < 100;
+                case "$100 - $150":
+                    return price >= 100 && price < 150;
+                case "$150-$300":
+                    return price >= 150 && price < 300;
+                case "$300+":
+                    return price >= 300;
+                default:
+                    return true; // If no range is selected or if the range is not recognized, ignore the price filter
+            }
+        }
+
+
+
+    }
+}
