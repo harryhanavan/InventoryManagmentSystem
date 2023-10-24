e52583e74b869d9a6305ab8067cd3b4d6660746a
harryhanavan <harryhanavan@gmail.com>
2023-10-24 17:03:44 +1100

    v1.11
    EOD upload

---
+++ InventoryManagementSystem/SupplierAdd.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class SupplierAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAddProduct = new Button();
+            this.lblSupplierAddress = new Label();
+            this.lblSupplierCategory = new Label();
+            this.lblSupplierPhone = new Label();
+            this.lblSupplierEmail = new Label();
+            this.lblSupplierName = new Label();
+            txtSupplierCategory = new TextBox();
+            txtSupplierID = new TextBox();
+            txtSupplierEmail = new TextBox();
+            txtSupplierPhone = new TextBox();
+            txtSupplierName = new TextBox();
+            this.lblSupplierID = new Label();
+            btnCancel = new Button();
+            this.txtSupplierCity = new TextBox();
+            lblSupplierCity = new Label();
+            txtSupplierAddress = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAddProduct
+            // 
+            btnAddProduct.Location = new Point(470, 330);
+            btnAddProduct.Name = "btnAddProduct";
+            btnAddProduct.Size = new Size(95, 23);
+            btnAddProduct.TabIndex = 25;
+            btnAddProduct.Text = "Add Product";
+            btnAddProduct.UseVisualStyleBackColor = true;
+            btnAddProduct.Click += btnAddSupplier_Click;
+            // 
+            // lblSupplierAddress
+            // 
+            this.lblSupplierAddress.AutoSize = true;
+            this.lblSupplierAddress.Location = new Point(287, 280);
+            this.lblSupplierAddress.Name = "lblSupplierAddress";
+            this.lblSupplierAddress.Size = new Size(100, 15);
+            this.lblSupplierAddress.TabIndex = 23;
+            this.lblSupplierAddress.Text = "Product Category";
+            // 
+            // lblSupplierCategory
+            // 
+            this.lblSupplierCategory.AutoSize = true;
+            this.lblSupplierCategory.Location = new Point(289, 251);
+            this.lblSupplierCategory.Name = "lblSupplierCategory";
+            this.lblSupplierCategory.Size = new Size(98, 15);
+            this.lblSupplierCategory.TabIndex = 22;
+            this.lblSupplierCategory.Text = "Product Quantity";
+            // 
+            // lblSupplierPhone
+            // 
+            this.lblSupplierPhone.AutoSize = true;
+            this.lblSupplierPhone.Location = new Point(307, 222);
+            this.lblSupplierPhone.Name = "lblSupplierPhone";
+            this.lblSupplierPhone.Size = new Size(78, 15);
+            this.lblSupplierPhone.TabIndex = 21;
+            this.lblSupplierPhone.Text = "Product Price";
+            // 
+            // lblSupplierEmail
+            // 
+            this.lblSupplierEmail.AutoSize = true;
+            this.lblSupplierEmail.Location = new Point(273, 193);
+            this.lblSupplierEmail.Name = "lblSupplierEmail";
+            this.lblSupplierEmail.Size = new Size(112, 15);
+            this.lblSupplierEmail.TabIndex = 20;
+            this.lblSupplierEmail.Text = "Product Description";
+            // 
+            // lblSupplierName
+            // 
+            this.lblSupplierName.AutoSize = true;
+            this.lblSupplierName.Location = new Point(301, 164);
+            this.lblSupplierName.Name = "lblSupplierName";
+            this.lblSupplierName.Size = new Size(84, 15);
+            this.lblSupplierName.TabIndex = 19;
+            this.lblSupplierName.Text = "Product Name";
+            // 
+            // txtSupplierCategory
+            // 
+            txtSupplierCategory.Location = new Point(406, 243);
+            txtSupplierCategory.Name = "txtSupplierCategory";
+            txtSupplierCategory.Size = new Size(121, 23);
+            txtSupplierCategory.TabIndex = 18;
+            // 
+            // txtSupplierID
+            // 
+            txtSupplierID.Location = new Point(406, 127);
+            txtSupplierID.Name = "txtSupplierID";
+            txtSupplierID.Size = new Size(121, 23);
+            txtSupplierID.TabIndex = 17;
+            // 
+            // txtSupplierEmail
+            // 
+            txtSupplierEmail.Location = new Point(406, 185);
+            txtSupplierEmail.Name = "txtSupplierEmail";
+            txtSupplierEmail.Size = new Size(121, 23);
+            txtSupplierEmail.TabIndex = 16;
+            // 
+            // txtSupplierPhone
+            // 
+            txtSupplierPhone.Location = new Point(406, 214);
+            txtSupplierPhone.Name = "txtSupplierPhone";
+            txtSupplierPhone.Size = new Size(121, 23);
+            txtSupplierPhone.TabIndex = 15;
+            // 
+            // txtSupplierName
+            // 
+            txtSupplierName.Location = new Point(406, 156);
+            txtSupplierName.Name = "txtSupplierName";
+            txtSupplierName.Size = new Size(121, 23);
+            txtSupplierName.TabIndex = 14;
+            // 
+            // lblSupplierID
+            // 
+            this.lblSupplierID.AutoSize = true;
+            this.lblSupplierID.Location = new Point(322, 135);
+            this.lblSupplierID.Name = "lblSupplierID";
+            this.lblSupplierID.Size = new Size(63, 15);
+            this.lblSupplierID.TabIndex = 13;
+            this.lblSupplierID.Text = "Product ID";
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(369, 330);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(95, 23);
+            btnCancel.TabIndex = 40;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // txtSupplierCity
+            // 
+            this.txtSupplierCity.Location = new Point(406, 301);
+            this.txtSupplierCity.Name = "txtSupplierCity";
+            this.txtSupplierCity.Size = new Size(121, 23);
+            this.txtSupplierCity.TabIndex = 41;
+            // 
+            // lblSupplierCity
+            // 
+            lblSupplierCity.AutoSize = true;
+            lblSupplierCity.Location = new Point(321, 309);
+            lblSupplierCity.Name = "lblSupplierCity";
+            lblSupplierCity.Size = new Size(64, 15);
+            lblSupplierCity.TabIndex = 42;
+            lblSupplierCity.Text = "Supplier ID";
+            // 
+            // txtSupplierAddress
+            // 
+            txtSupplierAddress.Location = new Point(406, 272);
+            txtSupplierAddress.Name = "txtSupplierAddress";
+            txtSupplierAddress.Size = new Size(121, 23);
+            txtSupplierAddress.TabIndex = 43;
+            // 
+            // SupplierAdd
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(txtSupplierAddress);
+            Controls.Add(lblSupplierCity);
+            Controls.Add(this.txtSupplierCity);
+            Controls.Add(btnCancel);
+            Controls.Add(btnAddProduct);
+            Controls.Add(this.lblSupplierAddress);
+            Controls.Add(this.lblSupplierCategory);
+            Controls.Add(this.lblSupplierPhone);
+            Controls.Add(this.lblSupplierEmail);
+            Controls.Add(this.lblSupplierName);
+            Controls.Add(txtSupplierCategory);
+            Controls.Add(txtSupplierID);
+            Controls.Add(txtSupplierEmail);
+            Controls.Add(txtSupplierPhone);
+            Controls.Add(txtSupplierName);
+            Controls.Add(this.lblSupplierID);
+            Name = "SupplierAdd";
+            Size = new Size(800, 450);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAddProduct;
+        private ComboBox cmbCategory;
+        private Label lblProductCategory;
+        private Label lblProductQuantity;
+        private Label lblProductPrice;
+        private Label lblProductDescription;
+        private Label lblProductName;
+        private TextBox txtSupplierCategory;
+        private TextBox txtSupplierID;
+        private TextBox txtSupplierEmail;
+        private TextBox txtSupplierPhone;
+        private TextBox txtSupplierName;
+        private Label lblProductID;
+        private Button btnCancel;
+        private TextBox txtSupplierIdOLD;
+        private Label lblSupplierCity;
+        private TextBox txtSupplierAddress;
+    }
+}
---
+++ InventoryManagementSystem/SupplierAdd.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace InventoryManagementSystem
+{
+    public partial class SupplierAdd : UserControl
+    {
+        public SupplierAdd()
+        {
+            InitializeComponent();
+            cmbCategory.DataSource = Enum.GetValues(typeof(SupplierCategory));
+
+        }
+        private void btnAddSupplier_Click(object sender, EventArgs e)
+        {
+            // Load the current list of suppliers
+            List<Supplier> suppliers = Supplier.LoadSuppliers();
+
+            // Get and validate inputs (you might want to add more validation here)
+            string name = txtSupplierName.Text;
+            string description = txtSupplierEmail.Text;
+
+            // Check for Empty Fields
+            if (string.IsNullOrWhiteSpace(txtSupplierID.Text) ||
+                string.IsNullOrWhiteSpace(txtSupplierName.Text) ||
+                string.IsNullOrWhiteSpace(txtSupplierEmail.Text) ||
+                cmbCategory.SelectedItem == null ||
+                string.IsNullOrWhiteSpace(txtSupplierQuantity.Text) ||
+                string.IsNullOrWhiteSpace(txtSupplierPrice.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            // 2. Validate Numerical Values
+            if (!int.TryParse(txtSupplierID.Text, out int supplierId) ||
+                !int.TryParse(txtSupplierQuantity.Text, out int quantity) ||
+                !decimal.TryParse(txtSupplierPrice.Text, out decimal price) ||
+                !int.TryParse(txtSupplierIdOLD.Text, out int supplierId))
+            {
+                MessageBox.Show("Please enter valid numerical values for Supplier ID, Quantity, Price, and Supplier ID.");
+                return;
+            }
+
+            // 3. Category Selection
+            string category = cmbCategory.SelectedItem.ToString();
+
+            // Create and add the supplier
+            Supplier newSupplier = new Supplier(supplierId, name, description, (SupplierCategory)Enum.Parse(typeof(SupplierCategory), category), price, quantity, supplierId);
+
+            // Add the new supplier to the list
+            bool success = Supplier.AddSupplier(newSupplier, suppliers);
+
+            if (success)
+                MessageBox.Show("Supplier added successfully!");
+            else
+                MessageBox.Show("An error occurred while adding the supplier.");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Main mainForm = (Main)ParentForm;
+            SupplierManagementForm supplierManagementForm = new SupplierManagementForm();
+            mainForm.LoadUserControl(supplierManagementForm);
+        }
+    }
+}
