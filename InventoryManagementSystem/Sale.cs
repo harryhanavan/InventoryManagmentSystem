7a48403859ebe00df3410a657b40d74ae5940c78
Dylan <dylanbracken12@gmail.com>
2023-10-25 17:13:51 +1100

    addes sales csv, loadSales

--- InventoryManagementSystem/Sale.cs
+++ InventoryManagementSystem/Sale.cs
@@ -14,6 +14,9 @@ namespace InventoryManagementSystem
         public DateTime Date { get; set; }
         public decimal TotalAmount { get; set; }
 
+        private static string dataDirectory = "../../../Data/";
+        private static string salesFilePath = Path.Combine(dataDirectory, "sales.csv");
+
         public Sale(int saleId, int productId, int quantitySold, DateTime date, decimal totalAmount)
         {
             SaleID = saleId;
@@ -37,10 +40,23 @@ namespace InventoryManagementSystem
             }
         }
 
-        public static List<Sale> ViewSales(List<Sale> sales)
+        public static List<Sale> loadSales(List<Sale> sales)
         {
-            // This method could be enhanced to filter sales based on dates, products, etc.
-            return sales;
+            List<Sale> salesList = new List<Sale>();
+            List<string[]> data = FileManager.ReadData(salesFilePath);
+
+            foreach (var record in data.Skip(1))
+            {
+                salesList.Add(new Sale(
+                    saleId: int.Parse(record[0]),
+                    productId: int.Parse(record[1]),
+                    quantitySold: int.Parse(record[2]),
+                    date: DateTime.Parse(record[3]),
+                    totalAmount: decimal.Parse(record[4])
+                ));
+            }
+
+            return salesList;
         }
     }
 }
